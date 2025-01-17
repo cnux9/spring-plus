e5ae1e213f82986d637795b80c3e6a77a5fdbd59
cnux9 <cnux9@naver.com>
2025-01-17 19:36:31 +0900

    feat: todo 다건 조회 날씨, 수정일 기간 검색 기능 구현

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -18,4 +19,20 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    @Query("""
+        SELECT t
+        FROM Todo t
+            LEFT JOIN FETCH t.user u
+        WHERE 
+            t.weather = :weather AND 
+            DATE(t.modifiedAt) BETWEEN :startDate AND :endDate
+        ORDER BY t.modifiedAt DESC
+    """)
+    Page<Todo> findAllByWeatherAndDate(
+            Pageable pageable,
+            @Param(value = "weather") String weather,
+            @Param(value = "startDate") LocalDate startDate,
+            @Param(value = "endDate") LocalDate endDate
+    );
 }

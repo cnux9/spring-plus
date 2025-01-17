4dc65102728f4a87ab56cbd5a803eb41a4900a67
cnux9 <cnux9@naver.com>
2025-01-17 16:19:47 +0900

    fix: 할일 생성 트랜잭션 읽기 전용 옵션 제거

--- src/main/java/org/example/expert/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/expert/domain/auth/controller/AuthController.java
@@ -1,7 +1,10 @@
 package org.example.expert.domain.auth.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.expert.config.JwtUtil;
 import org.example.expert.domain.auth.dto.request.SigninRequest;
 import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/auth/signup")
     public SignupResponse signup(@Valid @RequestBody SignupRequest signupRequest) {
@@ -23,7 +27,11 @@ public class AuthController {
     }
 
     @PostMapping("/auth/signin")
-    public SigninResponse signin(@Valid @RequestBody SigninRequest signinRequest) {
-        return authService.signin(signinRequest);
+    public SigninResponse signin(@Valid @RequestBody SigninRequest signinRequest, HttpServletResponse response) {
+        SigninResponse responseDto = authService.signin(signinRequest);
+
+        Cookie cookie = new Cookie("Authorization", responseDto.getBearerToken().substring(8));
+        response.addCookie(cookie);
+        return responseDto;
     }
 }
